681ff93057bd800c4f9d52da973a758df4c03764
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-10-23 15:33:35 +0100

    Added some default values for the user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -6,34 +6,49 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Your account needs a username'],
     unique: [true, 'Username already exists'],
   },
+
   profileImage: {
     type: String,
+    default:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
   },
+
   coverImage: {
     type: String,
+    default:
+      'https://i.pinimg.com/736x/5e/f9/a8/5ef9a872bcbc41da84c9ea86b16042bf.jpg',
   },
+
   bio: {
     type: String,
+    default: "I am so cool, I don't need a bio",
   },
+
   email: {
     type: String,
     required: [true, 'Please input your email'],
   },
+
   firstName: {
     type: String,
   },
+
   lastName: {
     type: String,
   },
+
   otherNames: {
     type: String,
   },
+
   followers: {
     type: [String],
   },
+
   following: {
     type: [String],
   },
+
   password: {
     type: String,
   },
