31c7aa5b79c3d2ce2a866459b49d066112041398
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-11-10 11:21:27 +0100

    Handling CORS

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://twitter-clone-vmwa.vercel.app'],
     methods: ['GET', 'POST', 'PATCH', 'UPDATE', 'DELETE'],
   },
 });
